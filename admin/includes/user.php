2b85e0b81ea0442a4c59ddf1456aebc02d0f4325
PaulmFfrench-dev <paul@brickfieldlabs.ie>
2020-07-27 16:43:05 +0100

    Add attribute finder method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -25,15 +25,26 @@ class User{
         return $result_set;
     }
 
-    public static function instantiation($found_user){
+    public static function instantiation($the_record){ //getting table
         $the_object = new self();
-            $the_object ->id         = $found_user ['id'];
-            $the_object ->username   = $found_user ['username'];
-            $the_object ->password   = $found_user ['password'];
-            $the_object ->first_name = $found_user ['first_name'];
-            $the_object ->last_name  = $found_user ['last_name'];
+            // $the_object ->id         = $found_user ['id'];
+            // $the_object ->username   = $found_user ['username'];
+            // $the_object ->password   = $found_user ['password'];
+            // $the_object ->first_name = $found_user ['first_name'];
+            // $the_object ->last_name  = $found_user ['last_name'];
+
+        foreach ($the_record as $the_attribute => $value) { //looping through the table and getting the key of the attribute
+            if($the_object->has_the_attribute($the_attribute)){ 
+                $the_object->the_attribute = $value; //if the key exists, the attribute of the object is getting it's value assigned to it.
+            }
+        }
 
             return $the_object;
+    }
+
+    private function has_the_attribute($the_attribute) {
+        $object_properties = get_object_vars($this);
+        array_key_exists($the_attribute, $object_properties); //two parameters, what we want to find and where to find it
     }
 }
 
