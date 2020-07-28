8d607c52f599afaec5762ca3c551a321193b0f36
PaulmFfrench-dev <paul@brickfieldlabs.ie>
2020-07-28 09:16:10 +0100

    Fix error where table value returns empty

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -25,14 +25,14 @@ class User{
         $the_object_array = array(); //empty array for objects
 
         while($row = mysqli_fetch_array($result_set)) { //loop that fetches the table and brings back result_set
-            $the_object_array[] = self::instantiation($row);
+            $the_object_array[] = self::instantation($row);
         }
 
         return $the_object_array; 
     }
 
-    public static function instantiation($the_record){ //loops through the attributes and assigns them to the objects properties
-        $the_object = new self();
+    public static function instantation($the_record){ //loops through the attributes and assigns them to the objects properties
+        $the_object = new self;
             // $the_object ->id         = $found_user ['id'];
             // $the_object ->username   = $found_user ['username'];
             // $the_object ->password   = $found_user ['password'];
@@ -41,7 +41,7 @@ class User{
 
         foreach ($the_record as $the_attribute => $value) { //looping through the table and getting the key of the attribute
             if($the_object->has_the_attribute($the_attribute)){ 
-                $the_object->the_attribute = $value; //if the key exists, the attribute of the object is getting it's value assigned to it.
+                $the_object->$the_attribute = $value; //if the key exists, the attribute of the object is getting it's value assigned to it.
             }
         }
 
@@ -50,7 +50,7 @@ class User{
 
     private function has_the_attribute($the_attribute) {
         $object_properties = get_object_vars($this);
-        array_key_exists($the_attribute, $object_properties); //two parameters, what we want to find and where to find it
+        return array_key_exists($the_attribute, $object_properties); //two parameters, what we want to find and where to find it
     }
 }
 
