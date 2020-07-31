3f0528d62e93c1659804cfeb800ab5c412e0a186
PaulmFfrench-dev <paul@brickfieldlabs.ie>
2020-07-31 10:13:58 +0100

    Move methods from parent class to child clas

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -16,9 +16,38 @@ class Photo extends Db_object {
     public $tmp_path;
     public $upload_directory =  "images";
     public $custom_errors = array();
+    public $upload_errors_array = array(
+
+        UPLOAD_ERR_OK   => "There is no error",
+        UPLOAD_ERR_INI_SIZE   => "The uploaded file exceeds the upload_max_filesize",
+        UPLOAD_ERR_FORM_SIZE   => "The uploaded file exceeds max file size",
+        UPLOAD_ERR_PARTIAL   => "The uploaded file was only partially uploaded",
+        UPLOAD_ERR_NO_FILE   => "No file was uploaded",
+        UPLOAD_ERR_NO_TMP_DIR   => "Missing a temporary folder",
+        UPLOAD_ERR_CANT_WRITE   => "Failed to write file to disk",
+        UPLOAD_ERR_EXTENSION   => "A PHP extension stopped the file upload"
+        
+        );
 
     // This is passing $_Files['uploaded_file'] as an arguement
+    public function set_file($file) {
 
+        if(empty($file) || !$file || !is_array($file)) {
+            $this->errors[] = "There was no file uploaded here";
+            return false;
+        } elseif($file['error'] !=0) {
+
+        $this->errors[] = $this->upload_errors_array[$file['error']];
+        return false;
+        
+        } else {
+        $this->user_image = basename($file['name']);
+        $this->tmp_path = ($file['tmp_name']);
+        $this->type = ($file['type']);
+        $this->size = ($file['size']);
+        }
+    }
+    
     public function picture_path() {
         return $this->upload_directory.DS.$this->filename;
     }
