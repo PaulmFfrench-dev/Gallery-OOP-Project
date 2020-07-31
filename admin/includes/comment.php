55f571b2cfa793eca09b611cd556c14d01f79ffd
PaulmFfrench-dev <paul@brickfieldlabs.ie>
2020-07-31 10:19:26 +0100

    Add find comments method

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -22,9 +22,14 @@ class Comment extends Db_object{
         }
     }
 
-    public static function find_the_comments($photo_id) {
-        $sql = "SELECT * FROM " . self::$db_table 
-        $sql .= . "WHERE photo_id = ". $photo_id
+    public static function find_the_comments($photo_id=0) { //passing in photo Id
+        $global $database
+        $sql  = "SELECT * FROM " . self::$db_table; 
+        $sql .= " WHERE photo_id = ". $database->escape_string($photo_id);
+        $sql .= "ORDER BY photo_id ASC";
+
+        return self::find_by_query($sql);
+
     }
 }
 
