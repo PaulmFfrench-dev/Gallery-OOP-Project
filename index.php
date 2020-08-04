6eb1f3787941d0dd6d70c68ef23d47c6ae4c8adb
PaulmFfrench-dev <paul@brickfieldlabs.ie>
2020-08-04 09:58:04 +0100

    Add functional pagination to index.php

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php include("includes/header.php"); ?>
 
 <?php 
-$photos = Photo::find_all();
+$page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
+$items_per_page= 4;
+$items_total_count = Photo::count_all();
+
+$paginate = new Paginate($page, $items_per_page, $items_total_count);
+
+$sql = "SELECT * FROM photos ";
+$sql .= "LIMIT {$items_per_page} ";
+$sql .= "OFFSET {$paginate->offset()}";
+$photos = Photo::find_by_query($sql);
 ?>
         <div class="row">
 
@@ -15,6 +24,20 @@ $photos = Photo::find_all();
                                 </a>
                             </div>
                     <?php endforeach; ?>
+                </div>
+                <div class="row">
+                    <ul class="pager">
+                        <?php 
+                            if($paginate->page_total() > 1) {
+                                if($paginate->has_next()) {
+                                    echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
+                                }
+                                if($paginate->has_previous()) {
+                                    echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
+                                }
+                            }
+                        ?>
+                    </ul>
                 </div>
             </div>
 
