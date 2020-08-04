5bbc842fbdb129f7d58e156d04c47327ec93f531
PaulmFfrench-dev <paul@brickfieldlabs.ie>
2020-08-04 11:49:46 +0100

    Set values for modal image variables

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,13 +1,12 @@
   </div>
     <!-- /#wrapper -->
-
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
     <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" ></script>
+
     <script src="js/scripts.js"></script>
 
     <script type="text/javascript">
